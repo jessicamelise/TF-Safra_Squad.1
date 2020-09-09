e3ad3f9af7b96a7a8fc9e529b4e2e04b293b5aca
jessicamelise <jessica.melise@hotmail.com>
2020-09-08 22:07:02 -0300

    Updates

---
+++ precache-manifest.ce49ebb45994055961bd4418350c82f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d5172220b4b763d042efe94e5d78b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "446944e8a7589dfbd8de",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "dc9f3a90b5ff738f3552",
+    "url": "/static/js/2.8f68b7e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f68b7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446944e8a7589dfbd8de",
+    "url": "/static/js/main.dcc6454e.chunk.js"
+  },
+  {
+    "revision": "82bebb97d91252e4eaa2",
+    "url": "/static/js/runtime-main.49b0ef31.js"
+  }
+]);
