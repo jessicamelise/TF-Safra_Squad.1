48b006ff571b52e3394ed12e886a959253ba198c
jessicamelise <jessica.melise@hotmail.com>
2020-09-11 14:48:47 -0300

    Updates

---
+++ precache-manifest.2320585a4e3f62d645e6dfd4596625ad.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb6e49c127811c63d583eee7b276330",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/index.html"
+  },
+  {
+    "revision": "b75ca3d8c504d48e6918",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/css/2.62afb475.chunk.css"
+  },
+  {
+    "revision": "ff12f45e38f50c65c49d",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/css/main.83d0bc3b.chunk.css"
+  },
+  {
+    "revision": "b75ca3d8c504d48e6918",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/js/2.df1f311a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/js/2.df1f311a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff12f45e38f50c65c49d",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/js/main.7f610679.chunk.js"
+  },
+  {
+    "revision": "223964e4f301830b6c3f",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/js/runtime-main.b9d9e8bb.js"
+  },
+  {
+    "revision": "18d452f542d12894b58de77e795bdf72",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/back.18d452f5.svg"
+  },
+  {
+    "revision": "f883bf0c87c6107391b075db90337de8",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/card-blue.f883bf0c.svg"
+  },
+  {
+    "revision": "19b994dd33704d1764ec26f3f8615989",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/card.19b994dd.svg"
+  },
+  {
+    "revision": "c8b1fdf3ca800c730d621508f97088bd",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/contact-blue.c8b1fdf3.svg"
+  },
+  {
+    "revision": "6b01b095da82669e0a63169d2bcb359e",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/contact.6b01b095.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f55e34d19bc2eba15c8c0be8806fca62",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/invoice-blue.f55e34d1.svg"
+  },
+  {
+    "revision": "c7fb71cb2421c465c248a6471b72736c",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/invoice.c7fb71cb.svg"
+  },
+  {
+    "revision": "a5ece4f732a70a5b84e19e9ff1fc5b5c",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/logo-safra-gold.a5ece4f7.svg"
+  },
+  {
+    "revision": "9c9f49e38774adbe85c73095d80e0468",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/logo-safra.9c9f49e3.svg"
+  },
+  {
+    "revision": "4f5acf35ad3561fd8528c5a15de5411e",
+    "url": "./https://github.com/jessicamelise/TF-Safra_Squad.1/static/media/logout.4f5acf35.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jessicamelise/TF-Safra_Squad.1/precache-manifest.44ef86bab9203d0335630ff4cd0cfe80.js"
+  "./https://github.com/jessicamelise/TF-Safra_Squad.1/precache-manifest.2320585a4e3f62d645e6dfd4596625ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jessicamelise/TF-Safra_Squad.1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./https://github.com/jessicamelise/TF-Safra_Squad.1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
