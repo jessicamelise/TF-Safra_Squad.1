65887c3a98fe06b95e1d51bad5de37e21231190a
jessicamelise <jessica.melise@hotmail.com>
2021-01-19 09:31:20 -0300

    alteração nas rotas

--- rendimentus/src/App.js
+++ rendimentus/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, HashRouter } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Profile } from "./components/profile/profile";
-
 import { BankStatement } from "./components/bankinformation/BankStatement.js";
 import { BankInvoice } from "./components/bankinformation/BankInvoice.js";
-// import { Products } from "colocar o caminho dos produtos";
 import { BankContact } from "./components/bankinformation/BankContact.js";
 import {Header} from"./components/header/index.js"
 import Footer from './components/footer';
@@ -14,19 +12,18 @@ import {ClientOffers} from "./components/offers/index.js"
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" exact={true} component={Home} />
         <Route path="/profile" component={Profile} /> 
         <Route path="/statement" component={BankStatement} />
         <Route path="/invoice" component={BankInvoice} />
-        {/* <Route path="/products" component={Products} /> */}
         <Route path="/contact" component={BankContact} />
         <Route path="/header" component={Header} />
         <Route path="/footer" component={Footer} />
         <Route path="/offers" component={ClientOffers} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
